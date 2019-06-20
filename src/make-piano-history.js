adacdcf07317af5f06149351f0beeed082e0d28d
Al Zimmerman <auraelius@gmail.com>
2019-06-19 21:45:53 -0700

    starting git operations

--- src/make-piano-history.js
+++ src/make-piano-history.js
@@ -1,8 +1,30 @@
 var shell = require('shelljs');
+let status = "";
 
-shell.echo('hello world');
+
+shell.echo('this script will collect all the piano records in history!');
  
 if (!shell.which('git')) {
-  shell.echo('Sorry, this script requires git');
+  console.log('Sorry, this script requires git');
+  shell.exit(1);
+}
+
+status = shell.cd('backend/data');
+
+if (status.stder && status.stderr.includes("cd: no such file or directory")) {
+  console.log('Sorry, this script needs the backend git repo to be present');
+  shell.exit(1);
+}
+
+// TODO
+/*
+get all commits starting with "c42f2bf fixed typos" and later
+capture in an array
+
+*/
+
+// Run external tool synchronously
+if (shell.exec('git log --oneline').code !== 0) {
+  shell.echo('Error: Git commit failed');
   shell.exit(1);
 }
